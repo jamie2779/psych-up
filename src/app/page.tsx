9c3733042592f2aa75faacea051a88e685a92a72
jamie2779 <jamie2779@naver.com>
2025-02-02 08:30:39 +0900

    feat: 커스텀 테마 오류로 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
-import Image from "next/image";
+import NextImage from "next/image";
+
+import { Image, Box, Button } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
-      <Image src="/Logo.svg" alt="Psych-up Logo" width={689} height={270} />
+      <Box color="psychup.primary" fontFamily="body">
+        Hello World
+      </Box>
+      <Button colorPalette={"success"}>Success 버튼</Button>
+
+      <Image asChild>
+        <NextImage
+          src="/Logo.svg"
+          alt="Psych-up Logo"
+          width={689}
+          height={270}
+        />
+      </Image>
     </div>
   );
 }
--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -1,18 +1,14 @@
 "use client";
 
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { ThemeProvider } from "next-themes";
-import { ReactNode } from "react";
 
-// 커스텀 테마 설정이 있다면 이곳에서 가져옵니다.
-import customTheme from "@/styles/theme";
-
-interface ProviderProps {
-  children: ReactNode;
+export default function RootLayout(props: { children: React.ReactNode }) {
+  return (
+    <ChakraProvider value={defaultSystem}>
+      <ThemeProvider attribute="class" disableTransitionOnChange>
+        {props.children}
+      </ThemeProvider>
+    </ChakraProvider>
+  );
 }
-
-const Provider = ({ children }: ProviderProps) => {
-  return <ChakraProvider value={customTheme}>{children}</ChakraProvider>;
-};
-
-export default Provider;
