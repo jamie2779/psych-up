e2e5b6d8f7c47b29eff65f96ae2f1a7fc4f21da8
jamie2779 <jamie2779@naver.com>
2025-02-14 03:40:51 +0900

    chore: NEXTAUTH 환경변수 설정

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,13 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
+  env: {
+    NEXTAUTH_URL:
+      process.env.NEXTAUTH_URL ||
+      (process.env.VERCEL_URL
+        ? `https://${process.env.VERCEL_URL}`
+        : "http://localhost:3000"),
+  },
 };
 
 export default nextConfig;
