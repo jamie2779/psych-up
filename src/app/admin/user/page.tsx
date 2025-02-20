37f1ef078892cc134efec98cd1a3c2431e58014f
jamie2779 <jamie2779@naver.com>
2025-02-21 04:48:59 +0900

    feat: 유저 오름차순 으로 정렬

--- src/app/admin/user/page.tsx
+++ src/app/admin/user/page.tsx
@@ -2,6 +2,10 @@ import AdminUser from "./page.client";
 import prisma from "@/lib/prisma";
 
 export default async function AdminUserPage() {
-  const users = await prisma.user.findMany();
+  const users = await prisma.user.findMany({
+    orderBy: {
+      memberId: "asc",
+    },
+  });
   return <AdminUser users={users} />;
 }
