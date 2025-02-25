d5cde039740d1bd1e6dca76d6cd9706dfcfe56a5
jamie2779 <jamie2779@naver.com>
2025-02-25 14:33:00 +0900

    feat:훈련목록 db연결

---
+++ src/app/dashboard/training/page.client.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { Box, Text, VStack } from "@chakra-ui/react";
+import TrainingList from "@/components/TrainingList";
+import { useRef } from "react";
+import { Scenario } from "@prisma/client";
+
+interface DashboardTrainingPageProps {
+  currentScenarioList: Scenario[];
+  scenarioList: Scenario[];
+}
+
+export default function DashboardTrainingPage({currentScenarioList, scenarioList}: DashboardTrainingPageProps) {
+  const trainingListRef = useRef<HTMLDivElement>(null);
+  return (
+    <Box h="100%">
+      <VStack pt={70} pb={30} px={45} spacing={6} align="flex-start">
+        <Text fontSize={32} fontWeight="medium" textStyle="gradient2">
+          어떤 훈련을 시작하시겠어요?
+        </Text>
+        <Text fontSize="m" color="grey.shade2" fontWeight="regular">
+          어떤것이든 바로 시작해보세요!
+        </Text>
+      </VStack>
+      <VStack maxW={1280} px={40} spacing={20}>
+        <TrainingList
+          title="진행 중인 훈련"
+          scenarioList={currentScenarioList}
+          showButton={true}
+          scrollToRef={trainingListRef}
+        />
+        <Box ref={trainingListRef}>
+          <TrainingList
+            title="전체 훈련 목록"
+            scenarioList={scenarioList}
+            showButton={false}
+          />
+        </Box>
+      </VStack>
+    </Box>
+  );
+}
